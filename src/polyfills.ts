cde3f6817fe8470063700ccc4a9d899da10166f3
amisha24 <amishasherekar24@gmail.com>
2025-10-03 16:33:51 +0100

    Final portfolio updates before deployment

---
+++ src/polyfills.ts
@@ -0,0 +1,16 @@
+/**
+ * This file includes polyfills needed by Angular and is loaded before the app.
+ * You can add your own extra polyfills to this file.
+ */
+
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+// Add global to window for some libraries
+(window as any).global = window;
+
+/***************************************************************************************************
+ * Zone JS is required by Angular itself.
+ */
+import 'zone.js';  // Included with Angular CLI.
